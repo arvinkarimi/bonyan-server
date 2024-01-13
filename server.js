43f8a1d0f6b2bb539ef617485c92247e4eff0f2d
arvin karimi <hayitsarvink@gmail.com>
2024-01-13 12:44:11 +0330

    v 1.3

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use( express.static("upload"))
+app.use( express.static("pdf"))
 const connectDB = async () => {
 
     try {
